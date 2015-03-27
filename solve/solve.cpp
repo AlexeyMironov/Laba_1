e4271c8cd9a880fedfebb1fc419bf35af57dee84
AlexeyMironov <idvreale@gmail.com>
2015-03-27 08:20:28 +0300

    выделил функцию нахождения квадратного уравнения

--- solve/solve.cpp
+++ solve/solve.cpp
@@ -6,54 +6,118 @@
 using namespace std;
 
 
+bool FindRoot(double a, double b, double c, double &x1, double &x2)
+{
+	double discriminant = b*b - 4 * a*c;
+
+	if (discriminant < 0)
+	{
+		return false;
+	}
+
+	else if (discriminant == 0)
+	{
+		x1 = -b / (2 * a);
+		return true;
+	}
+
+	else
+	{
+		discriminant = sqrt(discriminant);
+		x1 = (-b + discriminant) / (2 * a);
+		x2 = (-b - discriminant) / (2 * a);
+		return true;
+	}
+}
+
+bool CheckNumber(string N, double n)
+{
+	if ((istringstream(N) >> n >> ws).eof())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool TransformToDouble(string N, double &n, stringstream &nn)
+{
+	if ((istringstream(N) >> n >> ws).eof())
+	{
+		nn >> n;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "rus");
 
 	if (argc != 4)
 	{
-		cout << " оличество параметров не равно 4";
+		cout << "Ќеверно указан список параметров! (solve.exe <A> <B> <C>)";
 		return 1;
 	}
 
-	double x1 = 0, x2 = 0, a = 0, b = 0, c = 0;
 	string A(argv[1]), B(argv[2]), C(argv[3]);
+	double a = 0, b = 0, c = 0;
 
-	stringstream aa(A);
-	aa >> a;
+	stringstream aa;
+	if (!TransformToDouble(A, a, aa))
+	{
+		cout << "јргумент   " << A << "   не €вл€етс€ числом!" << endl;
+		system("pause");
+		return 1;
+	}
+
 	stringstream bb(B);
-	bb >> b;
+	if (!TransformToDouble(B, b, bb))
+	{
+		cout << "јргумент   " << B << "   не €вл€етс€ числом!" << endl;
+		system("pause");
+		return 1;
+	}
 	stringstream cc(C);
-	cc >> c;
+
+	if (!TransformToDouble(C, c, cc))
+	{
+		cout << "јргумент   " << C << "   не €вл€етс€ числом!" << endl;
+		system("pause");
+		return 1;
+	}
 
 	if (a == 0)
 	{
 		cout << "Ёто уравнение не €вл€етс€ квадратным";
-		return 0;
 	}
 
-	double discriminant = b*b - 4 * a*c;
-	if (discriminant < 0)
+	double x1, x2;
+	if (!FindRoot(a, b, c, x1, x2))
 	{
-		cout << "Ќет действительных корней уравнени€";
-		return 0;
+		cout << "Ёто уравнение не имеет действительных корней:" << endl;
 	}
 
-	if (discriminant == 0)
+	else
 	{
-		cout << "Ёто уравнение имеет единственный корень:" << endl;
-		x1 = -b / (2 * a);
-		cout << fixed << setprecision(4) << "x = " << x1;
-		return 0;
+		if (x1 == x2)
+		{
+			cout << "Ёто уравнение имеет единственный корень: " << x1 << endl;
+			cout << fixed << setprecision(4) << "x1 = " << x1 << endl;
+		}
+
+		else
+		{
+			cout << " орни уравнени€:" << endl;
+			cout << fixed << setprecision(4) << "x1 = " << x1 << "\n" << "x2 = " << x2 << endl;
+		}
 	}
 
-	if (discriminant > 0)
-	{
-		cout << " орни уравнени€:\n";
-		discriminant = sqrt(discriminant);
-		x1 = (-b + discriminant) / (2 * a);
-		x2 = (-b - discriminant) / (2 * a);
-		cout << fixed << setprecision(4) << "x1 = " << x1 << "\n" << "x2 = " << x2;
-		return 0;
-	}
+	system("pause");
+	return 0;
 }
